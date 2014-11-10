a346eb610cca1fb99d45d728f57389371f4231c0
osanchezf <osanchezf@users.noreply.github.com>
2014-11-10 18:29:37 -0500

    Update Panelist.php

--- Panelist.php
+++ Panelist.php
@@ -3,4 +3,11 @@
 // connect to the table panelists in the database triangle_test.
 
 class Panelist extends AppModel{
+  
+  public $belongsto = array(
+                    'Test' => array(
+                        'className' => 'Test',
+                        'foreingKey' => 'test_id'
+                    )
+    );  
 }
