58285c6438172f3edb826f8d9a138bb40e68aa63
osanchezf <osanchezf@users.noreply.github.com>
2014-11-10 18:38:00 -0500

    Update Test.php

--- Tests/Test.php
+++ Tests/Test.php
@@ -2,7 +2,7 @@
 
 class Test extends AppModel{ 
 
-    //Relate tests table with panelists table
+    //Relates tests table with panelists table
     public $hasMany = array(
         'Panelist' => array(
             'className'=>'Panelist',
