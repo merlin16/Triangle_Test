61da9c401aa13ebff83ea513931976244a56399c
osanchezf <osanchezf@users.noreply.github.com>
2014-11-17 00:10:47 -0500

    Update TestsController.php

--- Tests/TestsController.php
+++ Tests/TestsController.php
@@ -1,6 +1,7 @@
 <?php
 
-//Still in change. Not finished yet
+<?php
+
 class TestsController extends AppController{
     
     public $helpers = array('Html', 'Form'); 
@@ -8,42 +9,75 @@ class TestsController extends AppController{
     
     public function index()
     {
-        
+      
     }
     
     public function add()
     {
         if($this->request->is('post'))
-        {
-           $this->Test->create();
-           if($this->Test->save($this->request->data))
-            {   
-                $this->redirect(array('action' => 'view'));
-            } 
-            else 
+        {    
+            $testId = $this->request->data['Test']['id'];
+            
+            if(!$this->__validateId($testId))
             {
-                ///Comment
-            } 
-        }      
+                $this->Test->create();
+
+                if($this->Test->save($this->request->data))
+                {   
+                    $this->Session->setFlash(__("Test requirements have been saved!"));
+
+                    $totalPanelists = $this->request->data['Test']['total_panelist'];
+                    $this->__addPanelistData($testId, $totalPanelists);
+
+                    $this->redirect(array("action" => "viewPanelistInfo", "?" => array("testId" => $testId)));
+                } 
+                else 
+                {
+                    $this->Session->setFlash(__("Test requirements have not been saved!"));
+                }          
+             }   
+        }       
+    }
+    public function login()
+    {
+        
     }
     
-    public function view()
+    public function __validateId($testId)
     {
-        $numbers = [];  
+        $conditions = array('id' =>$testId);
         
-        //Generate table with Panelist Id, Same_1, Same_2 and Odd 
-        //for the total panelists entered in the form
+        if ($this->Test->hasAny($conditions))
+        {
+            $this->Session->setFlash(__("Sample Request Number " . $testId ." already exists"));
+            $this->redirect(array("action" => "add"));            
+        }
+    }
         
-        //Need to do code for total Panelist
-        for($i=0; $i<=5; $i++) // < total_panelist
+    public function viewPanelistInfo()
+    {        
+        $testId = $this->params['url']['testId'];
+                      
+       $data = $this->Test->Panelist->find('all', array ('fields' => array('test_id', 'panelist_id', 'same_1', 'same_2', 'odd'), 
+                                                         'conditions' => array('test_id' => $testId)));       
+       $this->set('tests', $data);       
+    }
+    
+    public function __addPanelistData($id, $totalPanelists)
+    {  
+                
+        $numbers = [];
+        
+        // Generate random numbers for total panelist
+        for($i=1; $i<=$totalPanelists; $i++)
         { 
             $array = [];
             $count = 0;
+
             while($count < 3) 
             {
                 $random = rand(100, 999);
-               
-               //If number is already in array it will not be entered. (Avoid number repetition)
+
                 if(!in_array($random, $array) && !in_array($random, $numbers))
                 {
                     $array[] = $random;
@@ -54,24 +88,21 @@ class TestsController extends AppController{
             for($j=0; $j<3; $j++)
             {
                 $numbers[] = $array[$j];
-            }  
-
-            //Add id, same_1, same_2 and odd to panelist table
-             $this->Test->Panelist->save(
+            }         
+            
+          // Save data for each panelist
+           $this->Test->Panelist->save(
                   array(
-                    'id' => ($i + 1),
-                    'test_id' => 20, //test_id
+                    'test_id' => $id,
+                    'panelist_id' => $i,
                     'same_1' => $array[0],
                     'same_2' => $array[1],
                     'odd' => $array[2]
                   )   
-              );                
-          } 
-      
-          $data = $this->Test->Panelist->find('all');                                                                                         
-          $this->set('panelists', $data);
-    }
-    
-    
+                );
+          }
+    } 
 }
+
+
 
